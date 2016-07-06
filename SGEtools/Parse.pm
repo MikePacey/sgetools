e1e41d26de2678d30d510b8b60734ebb2afda4e2
Pacey  Mike <pacey@wayland.private.dns.zone>
2016-07-06 09:31:52 +0100

    first batch of qtools

--- SGEtools/Parse.pm
+++ SGEtools/Parse.pm
@@ -1,3 +1,41 @@
-package SGEtools::Parse
+package SGEtools::Parse;
 
-# Placeholder
+use strict;
+use warnings;
+
+use XML::LibXML;
+
+our $VERSION;
+$VERSION = '1.0';
+
+our (@ISA, @EXPORT);
+
+BEGIN {
+  require Exporter;
+  @ISA = qw(Exporter);
+  @EXPORT = qw(SGEqstatf SGEqstatustar);
+}
+
+sub SGEqstatf {
+   my $ref;
+   my $fh;
+   my $parser;
+
+   open ($fh, "qstat -f -xml|");
+   $parser = XML::LibXML->new();
+   $ref = $parser->load_xml(IO => $fh);
+
+   return $ref;
+}
+
+sub SGEqstatustar {
+   my $ref;
+   my $fh;
+   my $parser;
+
+   open ($fh, "qstat -u \\\* -xml|");
+   $parser = XML::LibXML->new();
+   $ref = $parser->load_xml(IO => $fh);
+
+   return $ref;
+}
