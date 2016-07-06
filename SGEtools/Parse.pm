2e377b4049a1251937e1aaa07256e7b1c16c7cf5
Pacey  Mike <m.pacey@lancatser.ac.uk>
2016-07-06 10:37:14 +0100

    added POD

--- SGEtools/Parse.pm
+++ SGEtools/Parse.pm
@@ -1,5 +1,31 @@
 package SGEtools::Parse;
 
+=head1 SYNOPSIS
+
+ use SGEtools::Parse;
+
+ my ($queuelist, $joblist);
+ my $x;
+
+ $queuelist = SGEqstatf ();
+ $joblist   = SGEqstatustar ();
+
+ foreach $x ($ref->findnodes('/job_info/queue_info/Queue-List')) {
+    # Process job item
+ }
+
+=head1 DESCRIPTION
+
+SGEtools::Parse contains a set of helper functions which use XML::LibXML
+to return parsed XML output from (Son of) Grid Engine commands such as qstat.
+
+The returned parse can be interrogated using XML::LibXML functions such
+as findnodes() or findvalue()
+
+=head1 USAGE
+
+=cut
+
 use strict;
 use warnings;
 
@@ -16,6 +42,18 @@ BEGIN {
   @EXPORT = qw(SGEqstatf SGEqstatustar);
 }
 
+=pod
+
+=over
+
+=item C<SGEqstatf()>
+
+Returns parsed output from a call to "qstat -f -xml"
+
+=back
+
+=cut
+
 sub SGEqstatf {
    my $ref;
    my $fh;
@@ -27,6 +65,18 @@ sub SGEqstatf {
 
    return $ref;
 }
+
+=pod
+
+=over
+
+=item C<SGEqstatustar()>
+
+Returns parsed output from a call to "qstat -u \* -xml"
+
+=back
+
+=cut
 
 sub SGEqstatustar {
    my $ref;
